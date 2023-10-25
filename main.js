38868c99c3b61bdb55eca6289b06d7fa8c1d0f2e
isadoralamasberton <134522532+isadoralamasberton@users.noreply.github.com>
2023-10-25 11:26:14 -0300

    clap

--- main.js
+++ main.js
@@ -2,5 +2,9 @@ alert('Olá mundo!')
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
-
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+function tocaSomClap(){
+    document.querySelector('#som_tecla_clap').play();
+}
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
